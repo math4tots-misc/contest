2c3b85c5f53050103fa12bba7fd6f087dcb405c9
math4tots <math4tots@gmail.com>
2014-07-31 13:08:59 -0700

    hr beadornaments

---
+++ hr/challenges/beadornaments.rb
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/beadornaments
+
+class BeadOrnamentsSolver
+  def initialize
+    @N = gets.to_i
+    @b = gets.split.map(&:to_i)
+  end
+  
+  def recurse
+    
+  end
+end
