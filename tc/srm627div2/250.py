a8fb8e59c72c24649862ed4b624bed87a5cc0501
math4tots <math4tots@users.noreply.github.com>
2014-08-03 17:44:07 -0700

    tc refactor -- plugins
    TopCoder plugins and stuff… changed the way I use TopCoder.

--- tc/srm627div2/250.py
+++
@@ -1,6 +0,0 @@
-class ManySquares:
-	def howManySquares(self,sticks):
-		counter = [0 for _ in range(1001)]
-		for stick in sticks:
-			counter[stick] += 1
-		return sum(count // 4 for count in counter)
