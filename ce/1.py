f4de01c8f96c11f864f3c2abde830db29c5f077d
Kyumin Kim <math4tots@users.noreply.github.com>
2014-12-12 10:39:15 -0800

    ce 1 py

--- ce/1.py
+++ ce/1.py
@@ -1,17 +1,14 @@
 from sys import argv
 
-f = open(argv[1],'r')
-
-def process(i):
-	return 'FB' if (i%C==0) else 'F' if (i%A==0) else 'B' if (i%B==0) else str(i)
-
-for line in f.readlines():
-	if line.strip() == '': continue
-	A, B, N = map(int,line.split())
-	C = A * B
-	print(' '.join(process(i) for i in range(1,N+1)))
-
-f.close()
-
-
+with open(argv[1], 'r') as f:
+  for line in f.read().splitlines():
+    if not line.strip():
+      break
+    X, Y, N = map(int, line.split())
+    print(' '.join(
+      'FB'   if n % X == n % Y == 0 else
+      'F'    if n % X == 0          else
+      'B'    if n % Y == 0          else
+      str(n)
+      for n in range(1,N+1)))
 
