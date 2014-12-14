513f69d0155739b05fbb6a205cb33ed07748170a
Kyumin Kim <math4tots@users.noreply.github.com>
2014-12-14 00:46:15 -0800

    deleted old lib code for cxx and java

--- libjava/javalib/src/math4tots/Comparator.java
+++
@@ -1,5 +0,0 @@
-package math4tots;
-
-abstract public class Comparator<T> extends BaseObject {
-	abstract public int compare(T a, T b);
-}
--- libjava/javalib/src/math4tots/Iterator.java
+++
@@ -1,7 +0,0 @@
-package math4tots;
-
-abstract public class Iterator<T> extends BaseObject {
-	abstract public T peek();
-	abstract public T next();
-	abstract public boolean hasNext();
-}
